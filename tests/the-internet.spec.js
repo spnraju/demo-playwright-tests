3bca759336ee1cdf0c8880d3c4e228e7db111430
spnraju <padmanabharaju.sagi@gmail.com>
2021-10-24 21:53:28 +0530

    fail the test

--- tests/the-internet.spec.js
+++ tests/the-internet.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('validate title', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/');
   const title = page.locator('.heading');
-  await expect(title).toHaveText('Welcome to the-internet');
+  await expect(title).toHaveText('Fail');
 });
